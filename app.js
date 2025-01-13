0d6eda45b2ff2880e51a21b1b6a3693f44ae1432
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2025-01-13 20:03:32 +0200

    swiper

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.fon-box').classList.add('animate');
 });
+
+const swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  },
+});
