0389f414a57c3e4334fb4f8083e1f4301a7ce27a
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2025-01-30 00:05:00 +0200

    css

--- app.js
+++ app.js
@@ -51,3 +51,12 @@ window.addEventListener('scroll', () => {
     image.style.transform = `translateY(${yOffset}px)`;
   });
 });
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 10) {
+    header.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.5)';
+  } else {
+    header.style.boxShadow = 'none';
+  }
+});
