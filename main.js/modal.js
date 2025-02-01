ec459a2d9f65eeebc11073554128b7da4209226c
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2025-02-01 19:39:39 +0200

    css js

--- main.js/modal.js
+++ main.js/modal.js
@@ -11,3 +11,17 @@ window.addEventListener('click', function (e) {
     document.getElementById('modal').style.display = 'none';
   }
 });
+
+// отправкa формы
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  emailjs
+    .sendForm('service_id', 'template_id', this, 'user_id')
+    .then(() => {
+      alert('Сообщение отправлено!');
+      document.getElementById('modal').style.display = 'none';
+    })
+    .catch(() => alert('Ошибка отправки.'));
+});
